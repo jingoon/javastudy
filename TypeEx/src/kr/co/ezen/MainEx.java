4db9fe130384b9ce5610aaab086364c3b8740534
jingoon <parkman108@gmail.com>
2020-10-16 17:47:37 +0900

    10월 16일 java수업 변수와 자료형

---
+++ TypeEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,56 @@
+package kr.co.ezen;
+
+public class MainEx {
+	public static void main(String[] args) {
+		
+		boolean b1 = true;
+		System.out.println("참이다: "+b1);
+		boolean b2 = false;
+		System.out.println("거짓이다: "+b2);
+		
+		char c1 = '한';
+		
+		byte b3 = -128;
+		byte b4 = 127;
+		
+		short s1 = -32738;
+		short s2 = 32767;
+		
+		int i1 = -2100000000;
+		int i2 = 2100000000;
+		
+		long l1 = -2200000000L;
+		long l2 = 2200000000L;
+		
+		float f1 = 3.14F;
+		double d1 = 3.1415926535;
+		
+		double p=355.00000;
+		double i=113.00000;
+		double pi = p / i;
+		
+		System.out.println(pi);
+		System.out.println(d1);
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ VariableEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,51 @@
+package kr.co.ezen;
+
+public class MainEx {
+	
+	public static void main(String[] args) {
+		
+		String msg = "where";
+		String msg2 = "hi";
+		String msg3 = "good";
+		String msg4 = "moning";
+		
+		System.out.println(msg);
+		System.out.println(msg2);
+		System.out.println(msg3);
+		System.out.println(msg4);
+		System.out.println(msg);
+		System.out.println(msg3);
+		System.out.println(msg2);
+		System.out.println(msg);
+		
+		String jin = "11";
+		int jin3 = 11;
+	
+		
+		System.out.println(jin+jin );
+		System.out.println(jin3+jin3);
+		
+		
+		String msg5 = "hello"; 
+		String msg6 = "what";
+		String msg7 = "where";
+		String msg8 = "hi";
+		String msg9 = "문자";
+		String a = "test";
+		String g = "경기도";
+		String f = "hello";
+	
+		// 변수 선언 공식
+		// 자료형 변수명 = 자료형에맞는값;
+		// 값에맞는자료형 변수명 = 값;
+		
+		long aaa = 2500000000L;
+		
+	
+		
+		
+		
+		
+	}
+
+}
