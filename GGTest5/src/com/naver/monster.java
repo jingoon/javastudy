f7c47ba6cc12bb9475d6f4b65e90d2d68dcc376d
jingoon <parkman108@gmail.com>
2020-11-10 09:43:01 +0900

    11.10. GGTest5

---
+++ GGTest5/src/com/naver/monster.java
@@ -0,0 +1,26 @@
+package com.naver;
+
+public class monster {
+	private int hp;
+
+	public monster(int hp) {
+		super();
+		this.hp = hp;
+	}
+
+	@Override
+	public String toString() {
+		return "monster [hp=" + hp + "]";
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	
+
+}
